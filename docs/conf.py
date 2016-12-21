517703f382ad2892cf0fc3eaedf2541df88e89c9
Simon Potter <simoncpotter@gmail.com>
2016-12-21 12:43:39 +0000

    rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -124,7 +124,10 @@ todo_include_todos = False
 def setup(app):
   app.add_stylesheet( "css/tab.css" )
 
-html_theme = 'alabaster'
+import sphinx_rtd_theme
+
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
