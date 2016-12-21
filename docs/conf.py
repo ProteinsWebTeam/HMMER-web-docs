dd51f971d6db1b0c142da02b88c2daa639179a7d
Simon Potter <simoncpotter@gmail.com>
2016-12-21 11:37:49 +0000

    added extension

--- docs/conf.py
+++ docs/conf.py
@@ -24,12 +24,13 @@
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.4'
+# needs_sphinx = '1.4'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.ifconfig',
     'sphinx.ext.autosectionlabel'
 ]
 
