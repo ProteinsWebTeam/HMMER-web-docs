10e8662186927c725b7a5d39775d5b6431943fc6
Simon Potter <simoncpotter@gmail.com>
2016-12-21 11:39:11 +0000

    removed extension

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.autosectionlabel'
+    'sphinx.ext.autosectionlabel',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
